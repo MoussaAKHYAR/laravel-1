d3bfbdbb294c751a1258e2c83f97ce2fef365112
Moussa Akhyar <moussa.akhyar@gcmi.ca>
2023-02-07 09:57:45 -0500

    naming route

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,12 +3,10 @@
 @section('title', 'Home')
     
 @section('content')
-    <div class="main">
         <h1>
             Bienvenue
         </h1>
         <p>
             Home page
         </p>
-    </div>
 @endsection
